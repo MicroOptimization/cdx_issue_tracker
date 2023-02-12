560a64a58d86d58e762c3213d3c43b9beeb8938d
MicroOptimization <jackiezhen538@gmail.com>
2023-02-12 18:39:05 -0500

    figured out global variables with context_processor to display python variables in my parent jinja template that doesn't have its own url

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, g
 from db_utils import Db_helper
 import secrets
 
 application = Flask(__name__)
 application.secret_key = secrets.token_urlsafe(16)
 
+@application.context_processor
+def inject_user():
+    return dict(username=session.get("username"))
+
+        
 @application.route("/", methods =["GET", "POST"])
 def main():
     if request.method == "POST":
@@ -15,7 +20,7 @@ def main():
         create_info = list(filter(None, create_info))
         
         dbh = Db_helper()     
-        if len(login_info) == 2:
+        if len(login_info) == 2: #we've identified that we're TRYING to log in
             res = dbh.login(login_info[0], login_info[1])
             print(type(res))
             if res[0]: #if login succeeded
@@ -26,24 +31,29 @@ def main():
                 session["logged_in"] = True
                 session["username"] = res[1]["username"]
                 session["user_id"] = res[1]["user_id"]
-                print(session["username"], session["user_id"])
-            else:
+
+                return redirect("/home") #lets you into the home page
+            else: #incorrect password or username.
                 print("login failed")
                 return redirect("/")
-        elif len(create_info) == 3:
-            if dbh.create_user(create_info[0], create_info[1], create_info[2]):
+        elif len(create_info) == 3: #we've identified that we're TRYING to make a new account
+            if dbh.create_user(create_info[0], create_info[1], create_info[2]): #dbh.create_user will return true if the account was made successfully
                 print("acc created successfully!")
             else:
                 print("username or email taken!")
             pass
-        
         return render_template("login.html")
     return render_template("login.html")
 
+@application.route("/logout")
+def logout():
+    session.clear()
+    application.secret_key = secrets.token_urlsafe(16) #so this resets the session
+    return redirect("/")
 
-@application.route("/home")
+@application.route("/home", methods= ["POST", "GET"])
 def home():
-    return render_template("home.html")
+    return render_template("home.html", username=session["username"])
 
 @application.route("/projects")
 def projects():
